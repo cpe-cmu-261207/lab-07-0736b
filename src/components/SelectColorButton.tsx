47b0a91c9d974deeee6a36d4fed2cf3b190c032e
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-09-02 18:20:35 +0700

    update: Cell, Header, Select, Utility, PPStore

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,3 +1,5 @@
+import { PixelPainterStore, setSelectColor } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
@@ -5,14 +7,20 @@ type SelectColorButtonProps = {
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
   //modify this function to highlight correctly
+
+  const state = PixelPainterStore.useState()
+
   const computeRingSize = () => {
+    if(state.SelectColor === color){
+      return "ring-8 ring-green-400"
+    }
+    else
     return ""
-    return "ring-8 ring-green-400"
   }
 
   return (
     <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color }} onClick={() => {setSelectColor(color)}}
     >
     </div>
   )
--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -2,7 +2,8 @@ import {Store} from 'pullstate'
 
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
-  canvas: string[][] 
+  canvas: string[][] ,
+  SelectColor: string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
@@ -17,6 +18,48 @@ const createEmptyCanvas = () => {
   return output
 }
 
+// random all color on canvas
+const randomColorCanvas = () => {
+  const colorPicker: string[] = ['#000000', '#804000', '#FE0000', '#FE6A00', '#FFD800', '#00FF01', '#FFFFFF', '#01FFFF', '#0094FE', '#0026FF', '#B100FE', '#FF006E']
+  const output: string[][] = []
+  for (let i=0; i<16; i++){
+    output[i] = []
+    for (let j=0; j<16; j++){
+      output[i].push(colorPicker[Math.floor(Math.random() * colorPicker.length)])
+    }
+  }
+  return output
+}
+
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  SelectColor: "#000000"
 })
+
+//set select color button
+export const setSelectColor = (color: string) => {
+  PixelPainterStore.update(state => {
+    state.SelectColor = color
+  })
+}
+
+// paint on canvas at cell
+export const setColorOnCell = (x : number, y : number) => {
+  PixelPainterStore.update(state => {
+    state.canvas[y][x] = state.SelectColor
+  })
+}
+
+// execute emptyCanvas
+export const clearCanvas = () => {
+  PixelPainterStore.update(state => {
+    state.canvas = createEmptyCanvas()
+  })
+}
+
+// execute randomColorCanvas
+export const randomCanvas = () => {
+  PixelPainterStore.update(state => {
+    state.canvas = randomColorCanvas()
+  })
+}
